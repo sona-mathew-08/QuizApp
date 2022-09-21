50f5910771bf0a364de89314f49f349d409e014f
Sona Mathew <sonamathewc@gmail.com>
2022-09-21 16:45:55 +0530

    initial commit

---
+++ lib/answer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(child: Text('Answer 2'), onPressed: null),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+          backgroundColor: Colors.amber[400],
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            Answer(),
+            Answer(),
+            Answer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
